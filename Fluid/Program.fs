2e9e8a217d6d5b60dd400f0cd64aa5aebe22ae64
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2019-02-12 16:20:28 +0000

    Use CompilerService to count declarations

--- Fluid/Program.fs
+++ Fluid/Program.fs
@@ -1,7 +1,41 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open Microsoft.FSharp.Compiler.SourceCodeServices
+open System.IO
+
+let countDecl (fileContents : FSharpImplementationFileContents) = 
+    let decl = fileContents.Declarations in
+    let rec checkDecl (decl : FSharpImplementationFileDeclaration) : int = 
+        match decl with 
+        | FSharpImplementationFileDeclaration.Entity (_entity, decls) -> 
+            printf "Entity\n"; 
+            List.sum (List.map checkDecl decls)
+        | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue _ -> 
+            printf "MemberOrFunctionOrValue"; 
+            1
+        | FSharpImplementationFileDeclaration.InitAction _ -> 
+            printf "InitAction"; 
+            1
+    in
+    let declCounts = List.map checkDecl decl in
+    List.sum declCounts
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let checker = FSharpChecker.Create(keepAssemblyContents=true) in
+    if Array.length argv = 0
+    then
+        fprintfn stderr "%s" "No arguments provided"
+        1
+    else
+        let filename = argv.[0] in
+        let projectOptions, errors = 
+            match Path.GetExtension filename with
+            | ".fsx" -> checker.GetProjectOptionsFromScript(filename, filename) |> Async.RunSynchronously
+            | ".fs" -> failwith "Unimplemented"
+            | ext -> failwithf "Unsupported File Extension %s" ext
+        in
+        let results = checker.ParseAndCheckProject(projectOptions) |> Async.RunSynchronously
+        in
+        printf "%d Error(s)\n" (Array.length results.Errors)
+        let declCounts = List.map countDecl results.AssemblyContents.ImplementationFiles in
+        printf "%d Declarations in the file(s)\n" (List.sum declCounts) 
+        0
---
+++ examples/Adder2.fsx
@@ -0,0 +1,8 @@
+let add7 x
+  = x + 7
+
+let add2_7 x y
+  = x + y + 7
+
+let add_7_7 x
+  = x + 7 + 7
