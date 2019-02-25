f6301f5f487e50a2791b817a884348f52f1a3e28
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2019-02-25 22:04:23 +0000

    Add unit test

---
+++ FluidTest/Tests.fs
@@ -0,0 +1,13 @@
+﻿namespace FluidTest
+
+module TypingTest =
+    open NUnit.Framework
+    open FluidTypes
+    open FluidTypes.Typing
+    open FsUnit
+
+    let mk_int i = Const (IntLiteral i)
+    let mk_equal_int i = App (App (Const (Binop EqualInt), Var "$this"), mk_int i)
+    [<Test>]
+    let ``Int Literals have correct types`` () =
+        infer_type empty_ctx (mk_int 42) |> should equal (Some (BaseType (TInt, (mk_equal_int 42))))
