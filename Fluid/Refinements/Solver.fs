aa12ba7410ab4429fd3d41553ca2c740eb2f5852
Fangyi Zhou <me@fangyi.io>
2019-03-01 22:52:56 +0000

    Simple Solver interfacing

---
+++ Fluid/Refinements/Solver.fs
@@ -0,0 +1,50 @@
+namespace FluidTypes
+
+module Solver =
+    open System.Diagnostics
+    
+    type SolverOptions = {
+        path: string;
+        options: string;
+    }
+    
+    let default_options: SolverOptions = {
+        path = "z3";
+        options = "";
+    }
+    
+
+    let run_solver (opt: SolverOptions) (formula : string) : string =
+        let tempfile = System.IO.Path.GetTempFileName () in
+        let writer = new System.IO.StreamWriter (tempfile) in
+        writer.Write (formula);
+        writer.Close ();
+        (* http://www.fssnip.net/sw/title/RunProcess *)
+        let proc_info =
+            ProcessStartInfo(
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                FileName = opt.path,
+                Arguments = if opt.options <> "" then (tempfile + " " + opt.options) else tempfile
+        )
+        let outputHandler f (_sender:obj) (args:DataReceivedEventArgs) = f args.Data in
+        let outputs = ref [] in
+        let errors = ref [] in
+        let prepend ref_l v = ref_l := v :: !ref_l in 
+        let p = new Process(StartInfo = proc_info) in
+        p.OutputDataReceived.AddHandler (DataReceivedEventHandler (outputHandler (prepend outputs)));
+        p.ErrorDataReceived.AddHandler (DataReceivedEventHandler (outputHandler (prepend errors)));
+        let started = 
+            try
+                p.Start ()
+            with | ex ->
+                ex.Data.Add ("filename", opt.path)
+                reraise ()
+        if not started then
+            failwithf "Failed to start process %s" opt.path
+        p.BeginOutputReadLine ();
+        p.BeginErrorReadLine ();
+        p.WaitForExit ();
+        List.foldBack (fun acc v -> acc + "\n" + v) !outputs ""
+        
--- FluidTest/Tests.fs
+++ FluidTest/Tests.fs
@@ -11,3 +11,32 @@ module TypingTest =
     [<Test>]
     let ``Int Literals have correct types`` () =
         infer_type empty_ctx (mk_int 42) |> should equal (Some (BaseType (TInt, (mk_equal_int 42))))
+
+
+module CheckSolver =
+    open FluidTypes.Solver
+    open NUnit.Framework
+    open FsUnit
+    
+    let sample_formula_1 (* UNSAT *) = """(declare-const b Int)
+(declare-const x Int)
+(assert (= b (+ x 0)))
+(assert (not (= b x)))
+(check-sat)
+"""
+
+    let sample_formula_2 (* SAT *) = """(declare-const b Int)
+(declare-const x Int)
+(assert (= b (+ x 0)))
+(assert (= b x))
+(check-sat)
+"""
+    [<Test>]
+    let ``Sample Formula 1 is UNSAT`` () =
+        run_solver default_options sample_formula_1 |> should contain "unsat"
+        
+    [<Test>]
+    let ``Sample Formula 2 is SAT`` () =
+        let result = run_solver default_options sample_formula_2 in
+        result |> should contain "sat"
+        result |> should not' (contain "unsat")
