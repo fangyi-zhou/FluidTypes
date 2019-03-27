4a05f0b0a86ec01e819ab08a9a5608a5bfce2b3a
Fangyi Zhou <me@fangyi.io>
2019-03-27 16:22:33 +0000

    Extract consts correctly in Call and Const

--- Fluid/Extraction/Extraction.fs
+++ Fluid/Extraction/Extraction.fs
@@ -9,13 +9,24 @@ module Extraction =
     let rec extract_expr (e: FSharpExpr) : Term =
         match e with
         | BasicPatterns.Application (func_expr, type_args, arg_exprs) ->
-            printf "Application %A %A\n" func_expr arg_exprs;
             let func = extract_expr func_expr in
             let args = List.map extract_expr arg_exprs in
             List.fold (fun f a -> App (f, a)) func args
         | BasicPatterns.Call (obj_expr_opt, member_or_func, type_args1, type_args2, arg_exprs) ->
-            printf "Call %A %A %A\n" obj_expr_opt member_or_func arg_exprs;
-            let func = Var member_or_func.FullName in
+            let func =
+                match member_or_func.FullName with
+                | "Microsoft.FSharp.Core.Operators.( + )" -> Const (Binop Plus)
+                | "Microsoft.FSharp.Core.Operators.( - )" -> Const (Binop Minus)
+                | "Microsoft.FSharp.Core.Operators.( < )" -> Const (Binop Less)
+                | "Microsoft.FSharp.Core.Operators.( > )" -> Const (Binop Greater)
+                | "Microsoft.FSharp.Core.Operators.( = )" -> Const (Binop EqualInt) (* FIXME *)
+                | "Microsoft.FSharp.Core.Operators.( <> )" -> Const (Binop NotEqualInt) (* FIXME *)
+                | "Microsoft.FSharp.Core.Operators.( <= )" -> Const (Binop LessEqual)
+                | "Microsoft.FSharp.Core.Operators.( >= )" -> Const (Binop GreaterEqual)
+                | "Microsoft.FSharp.Core.Operators.( ~- )" -> Const (Unop Negate)
+                | "Microsoft.FSharp.Core.Operators.not" -> Const (Unop Not)
+                | name -> Var name
+            in
             let obj_opt = Option.map extract_expr obj_expr_opt in
             let args = List.map extract_expr arg_exprs in
             let func =
@@ -25,20 +36,19 @@ module Extraction =
             in
             List.fold (fun f a -> App (f, a)) func args
         | BasicPatterns.IfThenElse (guard_expr, then_expr, else_expr) ->
-            printf "IfThenElse %A %A %A\n" guard_expr then_expr else_expr;
             let cond = extract_expr guard_expr in
             let then_ = extract_expr then_expr in
             let else_ = extract_expr else_expr in
             IfThenElse (cond, then_, else_)
         | BasicPatterns.Lambda (lambda_var, body_expr) ->
-            printf "Lambda %A %A\n" lambda_var body_expr
             let body = extract_expr body_expr in
             Abs (lambda_var.FullName, body)
         | BasicPatterns.Const (const_value_obj, const_type) ->
-            printf "Const %A %A \n" const_value_obj const_type
-            Const (IntLiteral (const_value_obj :?> int))
+            match const_value_obj with
+            | :?int -> Const (IntLiteral (const_value_obj :?> int))
+            | :?bool -> Const (BoolLiteral (const_value_obj :?> bool))
+            | _ -> raise UnExtractable
         | BasicPatterns.Value (value_to_get) ->
-            printf "Value %A \n" value_to_get
             Var value_to_get.FullName
         | _ -> raise UnExtractable
 
@@ -49,8 +59,8 @@ module Extraction =
             match decl with
             | FSharpImplementationFileDeclaration.Entity (_entity, decls) ->
                 List.sumBy checkDecl decls
-            | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue (_, _, e) ->
-                printf "Extraction: %A\n" (extract_expr e);
+            | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue (member_or_func, arguments, e) ->
+                eprintf "Extraction: %A (%A) = %A\n" member_or_func arguments (extract_expr e);
                 1
             | FSharpImplementationFileDeclaration.InitAction _ ->
                 1
