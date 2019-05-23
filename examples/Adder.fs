05024b342253161ce189a2d45109cd353983ad12
Fangyi Zhou <me@fangyi.io>
2019-05-23 11:03:06 +0100

    Change examples

--- examples/Adder.fs
+++ examples/Adder.fs
@@ -6,3 +6,10 @@ open FluidTypes.Annotations
 let add7 x = x + 7
 
 let add7_anno (x : int) = x + 7
+
+let add2_7 x y = x + y + 7
+let add_7_7 x = x + 7 + 7
+
+let add3 x =
+    let two = 2
+    x + two
--- examples/Adder2.fs
+++
@@ -1,5 +0,0 @@
-module Adder2
-
-let add7 x = x + 7
-let add2_7 x y = x + y + 7
-let add_7_7 x = x + 7 + 7
