9add94e92db526fe600cb37a32bae2384a38047a
Fangyi Zhou <me@fangyi.io>
2019-05-23 15:04:03 +0100

    Make union example more complex

--- examples/Union.fs
+++ examples/Union.fs
@@ -3,11 +3,19 @@ module Union
 open FluidTypes.Annotations
 
 type Number =
-    | [<Refined("{v:int|v>=0}")>] NonNeg of int
+    | [<Refined("{v:int|v>0}")>] Pos of int
+    | [<Refined("{v:int|v=0}")>] Zero of int
     | [<Refined("{v:int|v<0}")>] Neg of int
 
 [<Refined("(x: Number) -> {v:int|v>=0}")>]
 let numberAbs (x: Number) =
     match x with
-    | NonNeg nonneg -> nonneg
-    | Neg neg -> neg
+    | Pos pos -> pos
+    | Zero zero -> zero
+    | Neg neg -> -neg
+
+let flip (x: Number) =
+    match x with
+    | Pos pos -> Neg -pos
+    | Zero zero -> Zero zero
+    | Neg neg -> Pos -neg
